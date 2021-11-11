8363ef879db510358b10e946d427f99d7268beaf
ScaMtares780CI <44785590+ScaMtares780CI@users.noreply.github.com>
2021-11-10 20:35:28 -0500

    Add files via upload

---
+++ Assets/Boundary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter(){
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Cannon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float speed = 4;
+
+    void LateUpdate()
+{
+        float x = Input.GetAxis("Mouse X") * 2;
+        float y = -Input.GetAxis("Mouse Y");
+        // vertical tilting
+        float yClamped = transform.eulerAngles.x + y;
+        transform.rotation = Quaternion.Euler(yClamped, transform.eulerAngles.y, transform.eulerAngles.z);
+        // horizontal orbiting
+        transform.RotateAround(new Vector3(0, 3, 0), Vector3.up, x);
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody projectile = Instantiate(projectile, transform.position, transform.rotation);
+            projectile.velocity = transform.forward * speed;
+        }
+    }
+
+}
